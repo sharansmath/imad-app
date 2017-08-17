7961740f6d6e3fb978a1038bf217039e563c66c9
sharansmath <sharan.math@gmail.com>
2017-08-17 22:53:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,13 +19,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-function hash(input) {
+function hash(input, salt) {
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    
+    return hashed.toString('hex');
 }
 
 app.get('/hash/:input', function(req, res) {
-    var hashedString = has(req.params.input);
+    var hashedString = has(req.params.input, 'some-randon-string');
     res.send(hashedString);
 })
 
