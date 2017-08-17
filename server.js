dd1f1a68ba926d503ecd603761a10662eca61739
sharansmath <sharan.math@gmail.com>
2017-08-17 23:54:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
 var bodyParser = require('body-parser'); 
+var session = require('express-session');
 
 var config = {
     user: 'mathsharan57',
@@ -16,6 +17,10 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
+app.use(session({
+    secret: 'somerandomvaluefornow',
+    cookie: {maxAge: 1000 * 60 * 60 * 60 * 24}
+}));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
