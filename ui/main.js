87a9a014542f1489aaa409a889a8b2f17dd1be2f
sharansmath <sharan.math@gmail.com>
2017-08-17 23:46:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,30 @@
-console.log('Loaded!');
+// Submit username and password
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
+    
+    // create a request object
+    var request = new XMLHttpRequest();
+    
+    // Capture the response and store it in a variable
+    request.onreadystatechange = function() {
+        // Take some action
+        if(request.status === 200) {
+            // Capture a list of names
+            console.log('user logged in');
+            alert('Logged in successfully');
+        } else (request.status === 403) {
+            alert('Username/password is incorrect')
+        } else (request.status === 5000) {
+            alert('Someothing went wrong');
+        }
+    };
+    
+    // Make the request
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST', 'http://mathsharan57.imad.hasura-app.io/lgin', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username: username, password: password}));
+};
